b5f28185c71ed48f6c98bcdc0b75db69547b2cee
wangkai <646651700@qq.com>
2019-03-31 18:14:00 +0800

    添加截图

---
+++ designPatterns/src/main/java/com/wufeng/singleton/hungry/HungryStaticSingleton.java
@@ -0,0 +1,17 @@
+package com.wufeng.singleton.hungry;
+
+/**
+ * Created by wangkai.
+ */
+
+//饿汉式静态块单例
+public class HungryStaticSingleton {
+    private static final HungryStaticSingleton hungrySingleton;
+    static {
+        hungrySingleton = new HungryStaticSingleton();
+    }
+    private HungryStaticSingleton(){}
+    public static HungryStaticSingleton getInstance(){
+        return  hungrySingleton;
+    }
+}
---
+++ designPatterns/src/main/java/com/wufeng/singleton/threadlocal/ThreadLocalSingleton.java
@@ -0,0 +1,20 @@
+package com.wufeng.singleton.threadlocal;
+
+/**
+ * Created by wangkai.
+ */
+public class ThreadLocalSingleton {
+    private static final ThreadLocal<ThreadLocalSingleton> threadLocalInstance =
+            new ThreadLocal<ThreadLocalSingleton>(){
+                @Override
+                protected ThreadLocalSingleton initialValue() {
+                    return new ThreadLocalSingleton();
+                }
+            };
+
+    private ThreadLocalSingleton(){}
+
+    public static ThreadLocalSingleton getInstance(){
+        return threadLocalInstance.get();
+    }
+}
